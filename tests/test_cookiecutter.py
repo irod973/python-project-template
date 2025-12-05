5645f76b78d1749eac36f8542b207420f292504a
Irving Rodriguez <irving.rodriguez973@gmail.com>
2025-12-04 21:54:46 -0800

    update tests

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -35,7 +35,7 @@ def test_project_generation(cookies: Cookies) -> None:
         "include_fastapi": "y",
         "include_metaflow": "y",
         "include_package": "y",
-        "coverage_threshold": "80"
+        "coverage_threshold": "1"
     }
     repository = context['name'].lower().replace(' ', '-')
     package = repository.replace('-', '_')
